b89b8cb8c4c64a3b2e2ec2598f4c453c0e23a14a
jason0860907 <61863553+jason0860907@users.noreply.github.com>
2024-06-03 15:18:29 +0800

    modified fastapi_test.py

--- fastapi_test.py
+++ fastapi_test.py
@@ -6,15 +6,20 @@ import io
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "FastAPI"}
 
 @app.post("/upload")
-async def create_upload_file(file: UploadFile):
-    model = YOLO("yolov8n.onnx")
+async def upload_file(file: UploadFile):
     request_object_content = await file.read()
     img = Image.open(io.BytesIO(request_object_content))
+    output_cls_box = detect(img)
+    return {"filename": file.filename, 'output_cls_box': output_cls_box}
+
+def detect(img):
+    model = YOLO("yolov8n.onnx")
     results = model(img)
     output_cls_box = []
     for i, r in enumerate(results):
@@ -22,6 +27,10 @@ async def create_upload_file(file: UploadFile):
         detected_box = r.boxes.xyxy.cpu().tolist()
         for i, (temp_cls, temp_box) in enumerate(zip(detected_cls, detected_box)):
             # print(r.names[int(temp_cls)], temp_box)
-            output_cls_box.append({"index": i, "class": r.names[int(temp_cls)], "x1": temp_box[0], "y1": temp_box[1], "x2": temp_box[2], "y2": temp_box[3]})
-            
-    return {"filename": file.filename, "output_cls_box": output_cls_box}
+            output_cls_box.append({"index": i, 
+                                    "class": r.names[int(temp_cls)], 
+                                    "x1": temp_box[0], 
+                                    "y1": temp_box[1], 
+                                    "x2": temp_box[2], 
+                                    "y2": temp_box[3]})
+    return output_cls_box

f9bb065bbf11822ab36075ee5a5bc36e96f24cb1
jason0860907 <61863553+jason0860907@users.noreply.github.com>
2024-06-03 14:04:50 +0800

    add fastapi_yolo.py file

---
+++ fastapi_test.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, UploadFile
+from ultralytics import YOLO
+from PIL import Image
+import io
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "FastAPI"}
+
+@app.post("/upload")
+async def create_upload_file(file: UploadFile):
+    model = YOLO("yolov8n.onnx")
+    request_object_content = await file.read()
+    img = Image.open(io.BytesIO(request_object_content))
+    results = model(img)
+    output_cls_box = []
+    for i, r in enumerate(results):
+        detected_cls = r.boxes.cls.cpu().tolist()
+        detected_box = r.boxes.xyxy.cpu().tolist()
+        for i, (temp_cls, temp_box) in enumerate(zip(detected_cls, detected_box)):
+            # print(r.names[int(temp_cls)], temp_box)
+            output_cls_box.append({"index": i, "class": r.names[int(temp_cls)], "x1": temp_box[0], "y1": temp_box[1], "x2": temp_box[2], "y2": temp_box[3]})
+            
+    return {"filename": file.filename, "output_cls_box": output_cls_box}
